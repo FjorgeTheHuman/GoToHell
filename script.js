fe537defbafadc1dbacebb00afe68de5721281ac
FjorgeTheHuman <30599715+FjorgeTheHuman@users.noreply.github.com>
2023-10-18 23:16:40 -0500

    debug: change order of rotation

--- script.js
+++ script.js
@@ -176,8 +176,8 @@ model.traverse((o) => {
 		model.scale.set(5, 5, 5);
 
 		// TODO Rotate model with thing
-		model.rotation.x = Math.PI / 2;
 		model.rotation.z = Math.PI;
+		model.rotation.x = Math.PI / 2;
 		//model.rotation.x = hdn;
 		// model.rotation.y = pitch;
 		// model.rotation.z = roll;

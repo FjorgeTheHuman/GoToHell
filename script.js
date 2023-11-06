b0e2c0e269711250bdd79b44ae39a0b08e74a0b0
FjorgeTheHuman <30599715+FjorgeTheHuman@users.noreply.github.com>
2023-11-05 20:35:55 -0600

    test: make top light less intense

--- script.js
+++ script.js
@@ -398,7 +398,7 @@ window.addEventListener("load", async () => {
 		arrowGroup.add(yellowLightBottom);
 
 		// Add a light to the group
-		const light = new THREE.PointLight(0x474747, 50);
+		const light = new THREE.PointLight(0x474747, 7);
 		light.position.set(0, 4, 3);
 		arrowGroup.add(light);
 

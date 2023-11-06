903e4c158cc28a47dd16f008e2c934e8b36c295e
FjorgeTheHuman <30599715+FjorgeTheHuman@users.noreply.github.com>
2023-11-05 20:01:16 -0600

    test: even more messing with lighting

--- script.js
+++ script.js
@@ -385,15 +385,15 @@ window.addEventListener("load", async () => {
 		arrowGroup.add(model);
 
 		// Add some yellow lights to the group
-		const yellowLightFront = new THREE.PointLight(0xfadb14, 5);
+		const yellowLightFront = new THREE.PointLight(0xffec3d, 5);
 		yellowLightFront.position.set(0, 5, 0);
 		arrowGroup.add(yellowLightFront);
 
-		const yellowLightTop = new THREE.PointLight(0xfadb14, 10);
+		const yellowLightTop = new THREE.PointLight(0xffec3d, 12);
 		yellowLightTop.position.set(0, 5, 2);
 		arrowGroup.add(yellowLightTop);
 
-		const yellowLightBottom = new THREE.PointLight(0xfadb14, 10);
+		const yellowLightBottom = new THREE.PointLight(0xffec3d, 12);
 		yellowLightBottom.position.set(0, 5, -2);
 		arrowGroup.add(yellowLightBottom);
 

9f020ec9935f5c6a082098c2c3279b439f4e573d
FjorgeTheHuman <30599715+FjorgeTheHuman@users.noreply.github.com>
2023-11-06 01:21:36 -0600

    fix: explicitly enable zoom

--- script.js
+++ script.js
@@ -300,13 +300,14 @@ window.addEventListener("load", async () => {
 	// Create the required three.js objects
 	const scene = new THREE.Scene();
 	let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 10.1);
-	camera.z = 0.1;
+	camera.z = 1;
 	const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	const controls = new OrbitControls(camera, document.getElementById('controls-overlay'));
 	controls.enabled = false;
 	controls.enablePan = false;
 	controls.enableRotate = false;
 	controls.enableDamping = true;
+	controls.enableZoom = true;
 	renderer.outputColorSpace = THREE.SRGBColorSpace;
 	const loader = new GLTFLoader();
 
